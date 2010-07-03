e28d55f5062ac6bcb03b589a4f8955928d5d3063
Nicolás Bello Camilletti <nbellocamilletti@gmail.com>
2010-07-02 21:53:10 -0300

    Agregada la versión simple del interprete de C, y algunas correciones del completo.

--- InterpreteC.lisp
+++ InterpreteC.lisp
@@ -1,55 +1,3 @@
-(defun asignacion (L mem)
-
-)
-(defun esVariable (A mem)
-    (not (null (buscar A mem)))
-)
-
-(defun esAsignacion (l mem)
-    (if (esVariable (car l) mem)
-        T
-        (if (or (eq (car l) '++) (eq (car l) '-- ))
-            T
-            nil
-    ) 
-)
-
-(defun ejec (prg ent mem &optional (sal nil) )
-    (if (null prg)
-        sal
-        (cond  
-            ((eq (caar prg) 'scanf) (ejec (cdr prg) (cdr ent) (modifMem (cadar prg) (car ent) mem) sal))
-            ((eq (caar prg) 'printf) (ejec (cdr prg) (cdr ent) mem (cons (eval (cadar prg) mem) sal )) ) ; esto imprime alrevez la salida
-            ((eq (caar prg) 'if) (if (not (eq (eval (cadar prg) mem) 0 ))
-                                    (ejec (append (nth 2 (car prg)) (cdr prg)) ent mem sal)
-                                    (if (eq (length (car prg)) 5)
-                                        (ejec (append (nth 4 (car prg)) (cdr prg)) ent mem sal)
-                                        (ejec (cdr prg) ent mem sal)
-                                    )
-                                 ))
-            ((eq (caar prg) 'while) (if (eq (eval (cadar prg) mem) 0)
-                                        (ejec (cdr prg) ent mem sal)
-                                        (ejec (append (nth 2 (car prg)) prg) ent mem sal)
-                                    ))
-        )
-    )
-)
-
-(defun run (prg ent &optional (mem nil) )
-    (if (null prg)
-        nil
-        (if (eq (caar prg) 'int)
-            (run (cdr prg) ent (agregarAMem (cdar prg) mem))
-            (if (eq (caar prg) 'main)
-                (ejec (cadar prg) ent mem)
-                'Error
-            )
-        )
-    )
-)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
 ;---------- Manejo de funciones y variables en memoria ---------
 
 (defun buscar (elemento listaPares)
@@ -160,7 +108,7 @@
         nil
         (cond 
             ((esVariable (car sentencia) mem) (cons (getVarValue (car sentencia) mem) (armarExpresion (cdr sentencia) mem ent sal) ))
-            ((esFun (car sentencia) mem) (cons (ejec (getFun (car sentencia) mem) ent mem ) (armarExpresion (cdr sentencia) mem ent sal) )) ;TODO sacar a una nueva funcion para poder llamar a armar expresion con los nuevos ent y sal
+            ((esFun (car sentencia) mem) (cons (ejecutar (getFun (car sentencia) mem) ent mem ) (armarExpresion (cdr sentencia) mem ent sal) )) ;TODO sacar a una nueva funcion para poder llamar a armar expresion con los nuevos ent y sal
             (T (cons (car sentencia) (armarExpresion (cdr sentencia) mem ent sal) ))
         )
     )
@@ -174,7 +122,7 @@
     ;operar elemento por elemento de la lista con apply.. son funciones lisp
 )
 
-(defun evaluarAux (listaParams) ; listaParams -> expresión,ent,sal
+(defun evaluar_Aux (listaParams) ; listaParams -> expresión,ent,sal
     (cons (operar (convertirALisp (car listaParams) ) ) (cdr listaParams) ) 
 )
 
@@ -182,21 +130,17 @@
     (evaluarAux (armarExpresion sentencia mem ent sal ))  
 )
 
-(defun asignacionAux (nombreVar listaParams mem) ; listaParams -> nuevoValor, ent y sal
+(defun asignacion_Aux (nombreVar listaParams mem) ; listaParams -> nuevoValor, ent y sal
     (cons (modifVarMem (list nombreVar (car listaParams)) mem) (caddr listaParams))
 )
 
 (defun asignacion (sentencia mem ent sal)
     (if (esVariable (car sentencia) mem)
-        (if (eq (nth 1 sentencia) '= ) 
-            (asignacionAux (car sentencia) (evaluar (nth 2 sentencia) mem ent sal) mem)
-            (if (eq (nth 1 sentencia) '++ )
-                (asignacion (list (car sentencia) '= (car sentencia) '+1 ) mem ent sal)
-                (if (eq (nth 1 sentencia) '-- )
-                    (asignacion (list (car sentencia) '= (car sentencia) '-1 ) mem ent sal)
-                    (asignacion (list (car sentencia) '= (car sentencia) (nth 1 sentencia) (nth 3 sentencia) ) mem ent sal)
-                )
-            )
+        (cond
+            ((eq (cadr sentencia) '= )  (asignacionAux (car sentencia) (evaluar (nth 2 sentencia) mem ent sal) mem) )
+            ((eq (cadr sentencia) '++ ) (asignacion (list (car sentencia) '= (car sentencia) '+1 ) mem ent sal))
+            ((eq (cadr sentencia) '-- ) (asignacion (list (car sentencia) '= (car sentencia) '-1 ) mem ent sal))
+            (t (asignacion (list (car sentencia) '= (car sentencia) (cadr sentencia) (nth 3 sentencia) ) mem ent sal))
         )
         (asignacion (reverse sentencia) mem ent sal )
     )
@@ -204,82 +148,91 @@
 ;----------fin de evaluacion de sentencias ---------
 
 ;----------Funciones------------
-(defun scanf (prg ent mem sal)
-    (ejec (cdr prg) (modifMem (cadar prg) (car ent) mem) (cdr ent)  sal)
+(defun ejecutar_scanf (prg ent mem sal)
+    (ejecutar (cdr prg) (modifMem (cadar prg) (car ent) mem) (cdr ent)  sal)
 )
 
-(defun printf (prg ent mem sal)
-    (ejec (cdr prg) mem (cdr ent) (append sal (list (evaluar (cadar prg) mem ent sal)) ));TODO ver lo que devuelve evaluar aca
+(defun ejecutar_printf (prg ent mem sal)
+    (ejecutar (cdr prg) mem (cdr ent) (append sal (list (evaluar (cadar prg) mem ent sal)) ));TODO ver lo que devuelve evaluar aca
 )
 
-(defun ifSentencia (prg ent mem sal)
+(defun ejecutar_if (prg ent mem sal)
     (if (not (eq (eval (cadar prg) mem) 0 ))
-        (ejec (append (nth 2 (car prg)) (cdr prg)) mem ent sal)
+        (ejecutar (append (nth 2 (car prg)) (cdr prg)) mem ent sal)
         (if (eq (length (car prg)) 5)
-            (ejec (append (nth 4 (car prg)) (cdr prg)) mem ent sal)
-            (ejec (cdr prg) mem ent  sal)
+            (ejecutar (append (nth 4 (car prg)) (cdr prg)) mem ent sal)
+            (ejecutar (cdr prg) mem ent  sal)
         )
     )
 )
 
-(defun while (prg ent mem sal)
+(defun ejecutar_while (prg ent mem sal)
     (if (eq (eval (cadar prg) mem) 0)
-        (ejec (cdr prg) mem ent sal)
-        (ejec (append (nth 2 (car prg)) prg) mem ent  sal)
+        (ejecutar (cdr prg) mem ent sal)
+        (ejecutar (append (nth 2 (car prg)) prg) mem ent  sal)
     )
 )
 
-(defun for (prg ent mem sal)
+(defun ejecutar_for (prg ent mem sal)
     ;(if (eq (eval (cadar prg) mem) 0)
-    ;    (ejec (cdr prg) ent mem sal)
-    ;    (ejec (append (nth 2 (car prg)) prg) ent mem sal)
+    ;    (ejecutar (cdr prg) ent mem sal)
+    ;    (ejecutar (append (nth 2 (car prg)) prg) ent mem sal)
     ;)
 )
 
-(defun ejecAsignacionAux (prg listaParams)
-    (ejec (cdr prg) (car listaParams) (cadr listaParams) (caddr listaParams) ) 
+(defun ejecutar_Asignacion_Aux (prg listaParams)
+    (ejecutar (cdr prg) (car listaParams) (cadr listaParams) (caddr listaParams) ) 
 )
 
-(defun ejecAsignacion (prg mem ent sal)
-    (ejecAsignacionAux prg (asignacion (car prg) mem ent sal))
+(defun ejecutar_Asignacion (prg mem ent sal)
+    (ejecutar_Asignacion_Aux prg (asignacion (car prg) mem ent sal))
 )
 
-(defun ejecFunAux (prg listaParams mem)
-    (ejec (cdr prg) mem (car listaParams) (cadr listaParams) ) 
+(defun ejecutar_Fun_Aux (prg listaParams mem)
+    (ejecutar (cdr prg) mem (car listaParams) (cadr listaParams) ) 
 )
 
-(defun ejecFun (prg ent mem sal)
-    (ejecFunAux prg (ejec (getFun (car sentencia) mem) mem ent sal ) mem)
+(defun ejecutar_Fun (prg ent mem sal)
+    (ejecutar_Fun_Aux prg (ejecutar (getFun (car sentencia) mem) mem ent sal ) mem);TODO falta evaluar parametros
 )
 
-(defun ejec (prg mem ent sal )
+(defun ejecutar (prg mem ent sal )
     (if (null prg)
-        (list ent sal)
-        (cond  
-            ((eq (caar prg) 'scanf) (scanf prg ent mem sal))
-            ((eq (caar prg) 'printf) (printf prg ent mem sal))
-            ((eq (caar prg) 'if) (ifSentencia prg ent mem sal))
-            ((eq (caar prg) 'while) (while prg ent mem sal))
-            ((eq (caar prg) 'for) (for prg ent mem sal))
-            ((esAsignacion (car prg) mem) (ejecAsignacion prg ent mem sal))
-            ((esFun (car prg) mem) (ejecFun (getFun (car sentencia) mem) ent mem sal))
-            (T 'Error)
+        (list ent sal '0)
+        (cond
+            ((eq (caar prg) 'scanf) (ejecutar_scanf prg ent mem sal))
+            ((eq (caar prg) 'printf) (ejecutar_printf prg ent mem sal))
+            ((eq (caar prg) 'if) (ejecutar_if prg ent mem sal))
+            ((eq (caar prg) 'while) (ejecutar_while prg ent mem sal))
+            ((eq (caar prg) 'for) (ejecutar_for prg ent mem sal))
+            ((eq (caar prg) 'return) (list ent sal (cadar prg) ) ;TODO ver que tengo que agregar una evaluación del parametro
+            ((esAsignacion (car prg) mem) (ejecutar_Asignacion prg ent mem sal))
+            ((esFun (car prg) mem) (ejecutar_Fun prg ent mem sal))
+            (t (list 'syntax_error  (car prg) ))
         )
     )
 )
 ;---------- Fin Funciones------------
 
+(defun agregarVarAMemMainAux ( prg mem salidaEvaluada )
+    (run (cdr prg) (cadr salidaEvaluada) (agregarVarAMem (car salidaEvaluada) mem) (caddr salidaEvaluada))
+)
+
+(defun agregarVarAMemMain ( prg mem ent sal )
+    (agregarVarAMemMainAux prg mem (evaluar (car prg) mem ent sal) )
+)
+
 ;---------- Main cicle -----------
-(defun run (prg ent &optional (mem nil) (sal nil))
+(defun run (prg &optional (mem nil) (ent nil) (sal nil))
     (if (null prg)
         nil
         (if (isFunctionDef (car prg))
             (if (isMain (car prg))
-                (cadr (ejec (cdr prg) mem ent sal))
-                (run (cdr prg) ent (agregarFunAMem (car prg) mem))
+                (cadr (ejecutar (cdr prg) mem ent sal));TODO falta evaluar parametros
+                (run (cdr prg) (agregarFunAMem (car prg) mem) ent sal)
             )
             (if (isVarDef (car prg))
-                (run (cdr prg) ent (agregarVarAMem (evaluar (car prg) mem ent sal) mem)) ;TODO ver lo que devuelve evaluar aca
+                (agregarVarAMemMain prg mem ent sal)
                 'Error
             )
         )
